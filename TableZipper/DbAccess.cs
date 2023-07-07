f12e03a419f9847e7bd508d30f65b6c149bb6063
mubsshir <khanmubsshir@gmail.com>
2023-07-07 14:57:03 +0530

    Add project files.

---
+++ TableZipper/DbAccess.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TableZipper
+{
+    class DbAccess
+    {
+        private static string ConnectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+        public static DataTable Get_table(int offset, int viewcount)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("USP_GET_MeterData", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@offset", offset);
+                    cmd.Parameters.AddWithValue("@viewcount", viewcount);
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    adapter.Fill(dt);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            return dt;
+        }
+    }
+}
---
+++ TableZipper/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.IO.Compression;
+using System.Threading;
+
+namespace TableZipper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Welcome\source\repos\TableZipper\TableZipper\ToZip\";
+            string zipPath = @"C:\Users\Welcome\Desktop\zips\";
+            int start = 0;
+            int end = 1000;
+            int fileIndex = 1;
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            if (!Directory.Exists(zipPath))
+            {
+                Directory.CreateDirectory(zipPath);
+            }
+            while (true)
+            {
+                DataTable network = DbAccess.Get_table(start, end);
+                if (network.Rows.Count == 0)
+                {
+                    break;
+                }
+                StringBuilder text = new StringBuilder("");
+                foreach (DataColumn dc in network.Columns)
+                {
+                    text.Append(dc.ColumnName + ",");
+                }
+
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(path+"data"+fileIndex.ToString()+".csv"))
+                    {
+                        sw.WriteLine(text.ToString().TrimEnd(','));
+                        foreach (DataRow dr in network.Rows)
+                        {
+                            StringBuilder row = new StringBuilder("");
+                            foreach (DataColumn dc in network.Columns)
+                            {
+                                row.Append(dr[dc] + ",");
+                            }
+                            sw.WriteLine(row.ToString().TrimEnd(','));
+                        }
+                        sw.Flush();
+                        sw.Close();
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }   
+                start += end ;
+                fileIndex++;
+            }
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            Thread.Sleep(3000);
+            ZipFile.CreateFromDirectory(zipPath, zipPath + "table.zip");
+
+        }
+    }
+}
